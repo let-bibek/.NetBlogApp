4d74844abdd2e22f995f546511136b5f6770c456
Bibek Shrestha <kumarbibek094@gmail.com>
2023-09-19 16:16:41 +0545

    initial setup

---
+++ CodePulse.API/Models/Domain/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace CodePulse.API.Models.Domain
+{
+	public class Category
+	{
+		public Guid Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string UrlHandler { get; set; }
+
+	}
+}
+
